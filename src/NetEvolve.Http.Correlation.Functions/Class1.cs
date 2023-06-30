080d46b3f8a9ff0bfa38b68c19640d280ea42cfe
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-06-30 22:47:30 +0200

    chore: Initial commit for `azure functions` support

---
+++ src/NetEvolve.Http.Correlation.Functions/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace NetEvolve.Http.Correlation.Functions;
+
+using Microsoft.Azure.WebJobs.Host;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class Class1 : IFunctionInvocationFilter
+{
+    /// <inheritdoc />
+    public Task OnExecutedAsync(FunctionExecutedContext executedContext, CancellationToken cancellationToken) => throw new System.NotImplementedException();
+
+    /// <inheritdoc />
+    public Task OnExecutingAsync(FunctionExecutingContext executingContext, CancellationToken cancellationToken) => Task.CompletedTask;
+}
