b67c40e9daf0237aff2b8df4db127afca8c9e63b
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-07-04 23:09:59 +0200

    chore: Added several Samples for WebApi

---
+++ sample/Sample.WebApi.NET7/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿namespace Sample.WebApi.NET7.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] _summaries = new[]
+    {
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching"
+    };
+
+    public WeatherForecastController() { }
+
+    [HttpGet]
+    [SuppressMessage(
+        "Security",
+        "CA5394:Do not use insecure randomness",
+        Justification = "As designed."
+    )]
+    public IEnumerable<WeatherForecast> Get() =>
+        Enumerable
+            .Range(1, 5)
+            .Select(
+                index =>
+                    new WeatherForecast
+                    {
+                        Date = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(index)),
+                        TemperatureC = Random.Shared.Next(-20, 55),
+                        Summary = _summaries[Random.Shared.Next(_summaries.Length)]
+                    }
+            )
+            .ToArray();
+}
---
+++ sample/Sample.WebApi.NET7/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Sample.WebApi.NET7;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using NetEvolve.Http.Correlation;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        _ = builder.Services.AddControllers();
+
+        // Add Http correlation support
+        _ = builder.Services.AddHttpCorrelation().WithGuidGenerator();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        _ = app.UseHttpCorrelation();
+
+        _ = app.UseHttpsRedirection();
+
+        _ = app.UseAuthorization();
+
+        _ = app.MapControllers();
+
+        app.Run();
+    }
+}
