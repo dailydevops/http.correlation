628f411706ce50fe9f969b5a929de56163d4acaa
Martin Stühmer <3283596+samtrion@users.noreply.github.com>
2023-08-29 12:48:22 +0200

    chore: Added `SequentialGuidOptions`

--- src/NetEvolve.Http.Correlation.Abstractions/Generators/SequentialGuidCorrelationIdProvider.cs
+++ src/NetEvolve.Http.Correlation.Abstractions/Generators/SequentialGuidCorrelationIdProvider.cs
@@ -1,5 +1,6 @@
 ﻿namespace NetEvolve.Http.Correlation.Generators;
 
+using Microsoft.Extensions.Options;
 using NetEvolve.Http.Correlation.Abstractions;
 using System;
 using System.Diagnostics.CodeAnalysis;
@@ -7,14 +8,18 @@ using System.Diagnostics.CodeAnalysis;
 /// <inheritdoc cref="IHttpCorrelationIdProvider" />
 internal sealed class SequentialGuidCorrelationIdProvider : IHttpCorrelationIdProvider
 {
-    private readonly SequentialType _sequentialType;
     private static readonly Random _random = Random.Shared;
+    private readonly IOptionsMonitor<SequentialGuidOptions> _options;
 
-    public SequentialGuidCorrelationIdProvider(SequentialType sequentialType) =>
-        _sequentialType = sequentialType;
+    public SequentialGuidCorrelationIdProvider(IOptionsMonitor<SequentialGuidOptions> options)
+        => _options = options;
 
     /// <inheritdoc />
-    [SuppressMessage("Security", "CA5394:Do not use insecure randomness", Justification = "As designed.")]
+    [SuppressMessage(
+        "Security",
+        "CA5394:Do not use insecure randomness",
+        Justification = "As designed."
+    )]
     public string GenerateId()
     {
         var timeStamp = DateTime.UtcNow.Ticks / 10000L;
@@ -28,13 +33,18 @@ internal sealed class SequentialGuidCorrelationIdProvider : IHttpCorrelationIdPr
         Span<byte> guidBytes = stackalloc byte[16];
         _random.NextBytes(guidBytes);
 
-        switch (_sequentialType)
+        var option = _options.CurrentValue;
+
+        switch (option.SequentialType)
         {
             case SequentialType.Sequantial1:
             case SequentialType.Sequantial2:
                 timeStampBytes[2..8].CopyTo(guidBytes[..6]);
 
-                if (_sequentialType == SequentialType.Sequantial2 && BitConverter.IsLittleEndian)
+                if (
+                    option.SequentialType == SequentialType.Sequantial2
+                    && BitConverter.IsLittleEndian
+                )
                 {
                     guidBytes[..4].Reverse();
                     guidBytes[4..6].Reverse();
---
+++ src/NetEvolve.Http.Correlation.Abstractions/Generators/SequentialGuidOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace NetEvolve.Http.Correlation.Generators;
+
+public class SequentialGuidOptions
+{
+    public SequentialType SequentialType { get; internal set; }
+}
