279bf4b0090398fd795fa2ede6692fa68948f552
Martin Stühmer <me@samtrion.net>
2024-05-17 15:00:27 +0200

    feat: Created Ulid CorrelationIdProvider

---
+++ src/NetEvolve.Http.Correlation.Ulid/ULIDCorrelationIdProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace NetEvolve.Http.Correlation;
+
+using System;
+using NetEvolve.Http.Correlation.Abstractions;
+
+/// <inheritdoc />
+internal sealed class UlidCorrelationIdProvider : IHttpCorrelationIdProvider
+{
+    /// <inheritdoc />
+    public string GenerateId() => Ulid.NewUlid().ToString();
+}
---
+++ tests/NetEvolve.Http.Correlation.Ulid.Tests.Unit/ULIDCorrelationIdProviderTests.cs
@@ -0,0 +1,38 @@
+﻿namespace NetEvolve.Http.Correlation.Ulid.Tests.Unit;
+
+using System.Linq;
+using Xunit;
+
+public class ULIDCorrelationIdProviderTests
+{
+    [Fact]
+    public void GenerateId_Fact_Expected()
+    {
+        // Arrange
+        var correlationIdProvider = new UlidCorrelationIdProvider();
+
+        // Act
+        var result = correlationIdProvider.GenerateId();
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+    [Fact]
+    public void GenerateId_UniqueIds_Expected()
+    {
+        // Arrange
+        const int numberOfIds = 1000;
+        var correlationIdProvider = new UlidCorrelationIdProvider();
+        var values = new string[numberOfIds];
+
+        // Act
+        foreach (var i in Enumerable.Range(0, numberOfIds))
+        {
+            values[i] = correlationIdProvider.GenerateId();
+        }
+
+        // Assert
+        Assert.Equal(numberOfIds, values.Distinct().Count());
+    }
+}
