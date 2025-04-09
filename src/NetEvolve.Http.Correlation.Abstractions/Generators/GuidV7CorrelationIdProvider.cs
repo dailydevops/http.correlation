d75f3414b3c36e7191d3576e032c953b7c73e669
Martin Stühmer <me@samtrion.net>
2025-04-09 09:10:37 +0200

    feat: Added support for guid v7

---
+++ src/NetEvolve.Http.Correlation.Abstractions/Generators/GuidV7CorrelationIdProvider.cs
@@ -0,0 +1,12 @@
+﻿#if NET9_0_OR_GREATER
+namespace NetEvolve.Http.Correlation.Generators;
+
+using System;
+using NetEvolve.Http.Correlation.Abstractions;
+
+internal sealed class GuidV7CorrelationIdProvider : IHttpCorrelationIdProvider
+{
+    /// <inheritdoc cref="IHttpCorrelationIdProvider.GenerateId" />
+    public string GenerateId() => Guid.CreateVersion7().ToString("N");
+}
+#endif
--- tests/NetEvolve.Http.Correlation.AspNetCore.Tests.Integration/HttpCorrelationMiddlewareTests.cs
+++ tests/NetEvolve.Http.Correlation.AspNetCore.Tests.Integration/HttpCorrelationMiddlewareTests.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using NetEvolve.SequentialGuid;
 using Xunit;
 
 public class HttpCorrelationMiddlewareTests : TestBase
@@ -23,6 +24,40 @@ public class HttpCorrelationMiddlewareTests : TestBase
 
         Assert.True(result.Headers.Contains(CorrelationConstants.HeaderName1));
 
+        var values = result.Headers.GetValues(CorrelationConstants.HeaderName1);
+        Assert.NotEmpty(values);
+        Assert.True(Guid.TryParse(values.First(), out _));
+    }
+
+#if NET9_0_OR_GREATER
+    [Fact]
+    public async Task UseHttpCorrelation_WithGuidV7Generator_Expected()
+    {
+        var result = await RunAsync(correlationBuilder: builder => builder.WithGuidV7Generator());
+        Assert.True(result.Headers.Contains(CorrelationConstants.HeaderName1));
+        var values = result.Headers.GetValues(CorrelationConstants.HeaderName1);
+        Assert.NotEmpty(values);
+        Assert.True(Guid.TryParse(values.First(), out _));
+    }
+#endif
+
+    [Theory]
+    [InlineData(SequentialGuidType.AsBinary)]
+    [InlineData(SequentialGuidType.AsString)]
+    [InlineData(SequentialGuidType.AtEnd)]
+    public async Task UseHttpCorrelation_WithSequentialGuidGenerator_Expected(
+        SequentialGuidType sequentialGuidType
+    )
+    {
+        var result = await RunAsync(
+#pragma warning disable CS0618 // Obsolete
+            correlationBuilder: builder =>
+            builder.WithSequentialGuidGenerator(options =>
+                options.SequentialType = sequentialGuidType
+            )
+#pragma warning restore CS0618 // Obsolete
+        );
+        Assert.True(result.Headers.Contains(CorrelationConstants.HeaderName1));
         var values = result.Headers.GetValues(CorrelationConstants.HeaderName1);
         Assert.NotEmpty(values);
         Assert.True(Guid.TryParse(values.First(), out _));
